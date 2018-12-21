43acfa74c269544d673f55135ea6f14b41fc4616
Sandy Macdonald <sandyjmacdonald@gmail.com>
2018-12-21 09:29:06 +0000

    Update keys.lua

--- sdcard/keys.lua
+++ sdcard/keys.lua
@@ -1,10 +1,6 @@
 require "keybow"
 require "layouts/default" -- Numberpad
 
-function setup()
-    keybow.load_pattern("static-tropical.png")
-end
-
 -- Custom layouts (uncomment to enable) --
 
 -- require "layouts/boilerplate" -- Handy bits of boilerplate text like Lorem Ipsum
