2475f4f1e256b0f2f35c7be7fd46a861a030b474
Phil Howard <phil@gadgetoid.com>
2020-06-24 13:10:34 +0100

    Add mouse layout to keys

--- sdcard/keys.lua
+++ sdcard/keys.lua
@@ -3,6 +3,7 @@ require "layouts/default" -- Numberpad
 
 -- Custom layouts (uncomment to enable) --
 
+-- require "layouts/mouse" -- Mouse keys
 -- require "layouts/boilerplate" -- Handy bits of boilerplate text like Lorem Ipsum
 -- require "layouts/lightroom" -- Handy hotkeys for Adobe Lightroom Classic CC
 -- require "layouts/pico8" -- Controls for Pico-8
