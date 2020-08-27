1c75df33b778562b98824d934ab002d851901384
Phil Howard <phil@gadgetoid.com>
2020-08-27 10:13:46 +0100

    Bump dependency versions
    bcm2835-1.58 -> bcm2835-1.68
    lua-5.3.5 -> lua-5.4.0

--- make.sh
+++ make.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 set -e
 
 WORKING_DIR=$(pwd)
 
-cd bcm2835-1.58
+cd bcm2835-1.68
 autoreconf -i
 ./configure --prefix=$(pwd)/build
 make
 make install
 cd $WORKING_DIR
 
-cd lua-5.3.5
+cd lua-5.4.0
 make linux
 cd $WORKING_DIR
 
